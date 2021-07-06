5c48a0cdfc67700178d5c1f54155a5eb63e39f01
Manuel Strebel <manuel.strebel@novu.ch>
2021-07-07 00:54:50 +0200

    load correct config

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 import path from "path";
 
 export default function nuxtBootstrapVue(moduleOptions) {
-  const options = Object.assign({}, this.options.axios, moduleOptions);
+  const options = Object.assign({}, this.options.cloudinaryImage, moduleOptions);
 
   // Register `plugin.js` template
   this.addPlugin({ src: path.resolve(__dirname, "plugin.js"), options });
